9a20802d0058d49c31be7364e7c1bc12ca1e0c42
vanpipka <vanpipka@gmail.com>
2019-07-30 21:56:21 +0300

    Dialog

--- main/models.py
+++ main/models.py
@@ -1323,7 +1323,7 @@ class MessageExpo(models.Model):
                     newMessage = MessageExpo.objects.create_message(data)
 
                     answer['status'] = True
-                    answer['message'] = {'id' = str(newMessage.id), 'created': newMessage.created.strftime("%d.%m.%Y %H:%M"), 'text': newMessage.text}
+                    answer['message'] = {'id': str(newMessage.id), 'created': newMessage.created.strftime("%d.%m.%Y %H:%M"), 'text': newMessage.text}
                 except Exception as e:
                     answer['errors'] = 'Не удалось сохранить сообщение'
 
