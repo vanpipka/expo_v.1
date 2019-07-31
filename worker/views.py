7a20f1d9c0ef3db21d8b0e70eb177615031459a2
vanpipka <vanpipka@gmail.com>
2019-07-31 17:18:39 +0300

    Photo

--- worker/views.py
+++ worker/views.py
@@ -46,9 +46,12 @@ def saveSettings(request):
 
                     #print(request.POST.__getitem__('data'))
 
-                    data = dict(json.loads(request.POST.__getitem__('data')))
-
-                    setWorker(request.user, data)
+                    try:
+                        data = dict(json.loads(request.POST.__getitem__('data')))
+                        setWorker(request.user, data)
+                    except Exception as e:
+                        print('Не удалось сохранить профиль')
+                        return JsonResponse({'status': False, 'errors': 'Не удалось сохранить профиль', 'csrfmiddlewaretoken': csrf.get_token(request)})
 
                 if request.is_ajax():
 
