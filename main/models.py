270378b74571c61411e717e81e34da688216cc91
vanpipka <vanpipka@gmail.com>
2019-11-16 13:15:19 +0300

    1

--- main/models.py
+++ main/models.py
@@ -559,7 +559,7 @@ class JobOrder(models.Model):
                     if response_array.count(e['jobOrder_id']) == 0:
                         response_array.append(e['jobOrder_id'])
 
-        objects = list(JobOrder.objects.all().select_related('city').select_related('company').order_by("-created").values('id', 'responseCount', 'description', 'date', 'place', 'created', 'company', 'city', 'city_id', 'city__name', 'company__name', 'company__image'))
+        objects = list(JobOrder.objects.all().select_related('city').select_related('company').order_by("-created").values('id', 'responseCount', 'description', 'date', 'enddate', 'place', 'created', 'company', 'city', 'city_id', 'city__name', 'company__name', 'company__image'))
 
         id_jobs = []
 
