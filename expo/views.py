73ef460f243832aab7cf9d209ad10e139e7c21f0
vanpipka <vanpipka@gmail.com>
2019-11-18 22:44:04 +0300

    1

--- expo/views.py
+++ expo/views.py
@@ -731,6 +731,9 @@ def infojobs(request):
         if jobInfo == False:
             return render(request, 'errors/500.html', status=500)
 
+        if jobInfo.author == request.user:
+            return render(request, 'NewJob.html', {'n': jobInfo, 'userType': userType, 'citylist': getCityListFull(), 'professionsList': getProfessionList()})
+
         return render(request, 'infojob.html', {'n': jobInfo, 'userType': userType, 'citylist': getCityListFull(), 'professionsList': getProfessionList()})
 
     else:
