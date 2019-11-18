e475309ad7fd4dde457d80df81684c94e0940519
vanpipka <vanpipka@gmail.com>
2019-11-18 20:09:42 +0300

    1

--- main/models.py
+++ main/models.py
@@ -622,7 +622,7 @@ class JobOrder(models.Model):
 
                 print('получаем отклики по заказу '+id)
 
-                rrr = JobResponse.objects.filter(JobOrder.objects.get(id=id))
+                rrr = JobResponse.objects.filter(jobOrder = JobOrder.objects.get(id=id))
 
                 print('1212')
                 #response_list = list(.values('description', 'worker'))
