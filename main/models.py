6845a88aea2b221b097522caf30426c355e6dadb
vanpipka <vanpipka@gmail.com>
2020-01-25 20:22:36 +0300

    1

--- main/models.py
+++ main/models.py
@@ -685,10 +685,7 @@ class JobOrder(models.Model):
             if (userType != 1 ):
                 e['response_is_available'] = 2      #ничего не выводить
             else:
-                print(e['id'])
-                response_list = list(JobResponse.objects.filter(worker=worker).values('status'))
-
-                print(1)
+                response_list = list(JobResponse.objects.filter(jobOrder = e).filter(worker=worker).values('status'))
                 e['response_is_available'] = 0
                 if len(response_list) == 0:
                     e['response_is_available'] = 1      #Можно откликнуться
